bafd7b07bdb9c97c88f47fa82ecfc61f94a20f7f
blaws006 <blaws006@gmail.com>
2023-10-14 13:37:30 -0400

    Added header, footer and campsitelist components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import React from 'react';
 import './App.css';
-import { Container,Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
+import Header from './components/Header';
+import CampsitesList from './features/campsites/CampSitesList';
+import Footer from './components/Footer'
 
 function App() {
+ 
   return (
     <div className="App">
-      <Navbar dark color='primary' sticky='top' expand='md'>
-      <Container>
-        <NavbarBrand href='/'>
-          <img src={NucampLogo} alt='Nucamp Logo' />
-        </NavbarBrand>
-      </Container>
-    </Navbar>
-    Ready for the workshop
+      <Header />
+      <CampsitesList />
+      <Footer />
     </div>
   );
 }
