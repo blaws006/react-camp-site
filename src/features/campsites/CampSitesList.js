5656bf7da4a4aa2bcedddb74d557f36bddb7b5df
blaws006 <blaws006@gmail.com>
2023-10-18 23:41:25 -0400

    can get a campsite by id on the directory page

--- src/features/campsites/CampSitesList.js
+++ src/features/campsites/CampSitesList.js
@@ -2,13 +2,13 @@ import CampsiteCard from "./CampSiteCard";
 import {Col, Row} from 'reactstrap';
 import { selectAllCampsites } from "./campsitesSlice";
 
-const CampsitesList = () => {
+const CampsitesList = ({ setCampsiteId }) => {
     const campsite = selectAllCampsites();
     return(
         <Row className="ms-auto">
             {campsite.map((campsite) => {
                 return (
-                    <Col md='5' className="m-4" key={campsite.id}>
+                    <Col md='5' className="m-4" key={campsite.id} onClick={()=> setCampsiteId(campsite.id)}>
                     <CampsiteCard campsite={campsite}></CampsiteCard>
             </Col>)
             })}
