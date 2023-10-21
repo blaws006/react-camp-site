64d439d47f0b247491c88111971631d414565db5
blaws006 <blaws006@gmail.com>
2023-10-21 10:14:30 -0400

    Added some routing via id

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import { Route, Routes } from 'react-router-dom'
 import './App.css';
 
@@ -15,6 +16,7 @@ function App() {
           <Route path='/' element={<HomePage/>} />
           <Route path='contact' element={<ContactPage/>} />
           <Route path='directory' element={<CampsitesDirectoryPage/>} />
+          <Route path='directory/:campsiteId' element={<CampsiteDetailPage/>} />
         </Routes>
       <Footer />
     </div>
--- src/features/campsites/CampSitesList.js
+++ src/features/campsites/CampSitesList.js
@@ -2,14 +2,14 @@ import CampsiteCard from "./CampSiteCard";
 import {Col, Row} from 'reactstrap';
 import { selectAllCampsites } from "./campsitesSlice";
 
-const CampsitesList = ({ setCampsiteId }) => {
+const CampsitesList = () => {
     const campsite = selectAllCampsites();
     return(
         <Row className="ms-auto">
             {campsite.map((campsite) => {
                 return (
-                    <Col md='5' className="m-4" key={campsite.id} onClick={()=> setCampsiteId(campsite.id)}>
-                    <CampsiteCard campsite={campsite}></CampsiteCard>
+                    <Col md='5' className="m-4" key={campsite.id} >
+                    <CampsiteCard campsite={campsite} />
             </Col>)
             })}
     </Row>
