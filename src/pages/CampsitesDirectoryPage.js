99a684eb1660c8ca839b979207a3006d08864379
blaws006 <blaws006@gmail.com>
2023-10-21 11:47:19 -0400

    added breadcrumbs to page

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,10 +1,12 @@
 import { Container} from "reactstrap";
 import CampsitesList from '../features/campsites/CampSitesList';
+import SubHeader from '../components/SubHeader';
 
 const CampsitesDirectoryPage = () => {
     
     return (
         <Container>
+            <SubHeader current='Directory' />
             <CampsitesList />
         </Container>
     )
