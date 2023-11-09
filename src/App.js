b9ce26a81e17f259800a848277e3a59aa6f23305
blaws006 <blaws006@gmail.com>
2023-11-08 23:21:56 -0500

    Other components display the db.json data

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import AboutPage from './pages/AboutPage';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
+import { fetchPartners } from './features/partners/partnersSlice';
+import { fetchPromotions } from './features/promotions/promotionsSlice';
 import './App.css';
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
  const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchCampsites());
+    dispatch(fetchPartners());
+    dispatch(fetchPromotions());
   }, [dispatch]);
 
   return (
--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -5,6 +5,8 @@ import AnimatedDisplayCard from './AnimatedDisplayCard';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 import { selectFeaturedPartner } from '../partners/partnersSlice';
+import Error from '../../components/Error';
+import Loading from '../../components/Loading';
 
 const DisplayList = () => {
     const items = useSelector((state) => [selectFeaturedCampsite(state), selectFeaturedPromotion(state), selectFeaturedPartner(state)]);
@@ -12,9 +14,18 @@ const DisplayList = () => {
     return (
         <Row>
             {items.map((item, idx) => {
-                return(item && (
+                const {featuredItem, isLoading, errMsg} = item;
+                if (isLoading) {
+                    return <Loading key={idx} />
+                }
+                if (errMsg) {
+                    return <Error key={idx}  errMsg={errMsg} />
+                }
+
+                return(
+                    featuredItem && (
                     <Col md className='m-1' key={idx}>
-                        <AnimatedDisplayCard item={item} />
+                        <AnimatedDisplayCard item={featuredItem} />
                     </Col>
                 ))
             })}
